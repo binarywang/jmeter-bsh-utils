464982d2d30263e7c057ee823da26c2c3f470c20
Binary Wang <binarywang@gmail.com>
2016-11-02 14:25:30 +0800

    方法异常在内部处理，因抛出后jmeter无法处理

--- src/main/java/com/github/binarywang/bsh/BshUtils.java
+++ src/main/java/com/github/binarywang/bsh/BshUtils.java
@@ -1,6 +1,5 @@
 package com.github.binarywang.bsh;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
@@ -38,46 +37,50 @@ public class BshUtils {
     }
 
     public static void checkState(Logger log, JMeterVariables vars, String path,
-            String flowId, int sleepInterval)
-            throws UnsupportedEncodingException, InterruptedException {
-        while (true) {
-            log(log, vars, "Will be sleeping for the next " + sleepInterval
-                + " seconds..............");
-            Thread.sleep(sleepInterval * 1000);
-            log(log, vars, "Wakes up now and continues to work");
-            log(log, vars,
-                String.format("path=[%s],flowId=[%s]", path, flowId));
+            String flowId, int sleepInterval) {
+        try {
+            while (true) {
+                log(log, vars, "Will be sleeping for the next " + sleepInterval
+                    + " seconds..............");
+                Thread.sleep(sleepInterval * 1000);
+                log(log, vars, "Wakes up now and continues to work");
+                log(log, vars,
+                    String.format("path=[%s],flowId=[%s]", path, flowId));
 
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("id", flowId);
-            HttpResponse response = HttpRequest.post(path)
-                .header("User-Agent", BshUtils.class.getName())
-                .body(jsonObject.toString().getBytes(StringPool.UTF_8),
-                    "application/json; charset=utf-8")
-                .send();
-            log(log, vars, "================>\n   " + response.bodyText());
-            Boolean success = JsonPath.read(response.bodyText(), "$.success");
-            if (!success
-                || JsonPath.read(response.bodyText(), "$.result") == null) {
-                log(log, vars, "查询流程状态时发生错误");
-                break;
-            }
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id", flowId);
+                HttpResponse response = HttpRequest.post(path)
+                    .header("User-Agent", BshUtils.class.getName())
+                    .body(jsonObject.toString().getBytes(StringPool.UTF_8),
+                        "application/json; charset=utf-8")
+                    .send();
+                log(log, vars, "================>\n   " + response.bodyText());
+                Boolean success = JsonPath.read(response.bodyText(),
+                    "$.success");
+                if (!success
+                    || JsonPath.read(response.bodyText(), "$.result") == null) {
+                    log(log, vars, "查询流程状态时发生错误");
+                    break;
+                }
 
-            String jsonPath = "$.result.flowNodes[0].runtimeState";
-            Integer state = JsonPath.read(response.bodyText(), jsonPath);
-            if (state != 2) {
-                log(log, vars, "G-SA-WX is still ongoing");
+                String jsonPath = "$.result.flowNodes[0].runtimeState";
+                Integer state = JsonPath.read(response.bodyText(), jsonPath);
+                if (state != 2) {
+                    log(log, vars, "G-SA-WX is still ongoing");
+                    continue;
+                }
+
+                jsonPath = "$.result.flowNodes[1].graph.grapgNodes[2].runtimeState";
+                state = JsonPath.read(response.bodyText(), jsonPath);
+                if (state == 2) {
+                    break;
+                }
+
+                log(log, vars, "J-CREATE-INSTALLMENT-ORDER is still ongoing");
                 continue;
             }
-
-            jsonPath = "$.result.flowNodes[1].graph.grapgNodes[2].runtimeState";
-            state = JsonPath.read(response.bodyText(), jsonPath);
-            if (state == 2) {
-                break;
-            }
-
-            log(log, vars, "J-CREATE-INSTALLMENT-ORDER is still ongoing");
-            continue;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
