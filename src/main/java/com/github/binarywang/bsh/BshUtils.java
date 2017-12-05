0de0e1898f86c49e5a1a5e04f0c9f836c6fce5a9
Binary Wang <binarywang@gmail.com>
2017-12-05 12:21:18 +0800

    fix

--- src/main/java/com/github/binarywang/bsh/BshUtils.java
+++ src/main/java/com/github/binarywang/bsh/BshUtils.java
@@ -1,87 +1,85 @@
 package com.github.binarywang.bsh;
 
-import java.util.Iterator;
-import java.util.Map.Entry;
-
+import com.google.gson.JsonObject;
+import com.jayway.jsonpath.JsonPath;
+import jodd.http.HttpRequest;
+import jodd.http.HttpResponse;
+import jodd.util.StringPool;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.log.Logger;
 
-import com.google.gson.JsonObject;
-import com.jayway.jsonpath.JsonPath;
-
-import jodd.http.HttpRequest;
-import jodd.http.HttpResponse;
-import jodd.util.StringPool;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 /**
  * @author Binary Wang
  */
 public class BshUtils {
-    public static void printParameters(Logger log, JMeterVariables vars) {
-        log.info("vars = " + ToStringBuilder.reflectionToString(vars,
-            ToStringStyle.MULTI_LINE_STYLE));
+  public static void printParameters(Logger log, JMeterVariables vars) {
+    log.info("vars = " + ToStringBuilder.reflectionToString(vars,
+        ToStringStyle.MULTI_LINE_STYLE));
 
-        Iterator<Entry<String, Object>> it = vars.getIterator();
-        while (it.hasNext()) {
-            String msg = " [params]-> " + it.next().toString();
-            log(log, vars, msg);
+    Iterator<Entry<String, Object>> it = vars.getIterator();
+    while (it.hasNext()) {
+      String msg = " [params]-> " + it.next().toString();
+      log(log, vars, msg);
+    }
+  }
+
+  public static void log(Logger log, JMeterVariables vars, String msg) {
+    msg = vars.getThreadName() + ": " + msg;
+    log.info(msg);
+    System.out.println(msg);
+  }
+
+  public static void checkState(Logger log, JMeterVariables vars, String path,
+                                String flowId, int sleepInterval) {
+    try {
+      while (true) {
+        log(log, vars, "Will be sleeping for the next " + sleepInterval
+            + " seconds..............");
+        Thread.sleep(sleepInterval * 1000);
+        log(log, vars, "Wakes up now and continues to work");
+        log(log, vars,
+            String.format("path=[%s],flowId=[%s]", path, flowId));
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("id", flowId);
+        HttpResponse response = HttpRequest.post(path)
+            .header("User-Agent", BshUtils.class.getName())
+            .body(jsonObject.toString().getBytes(StringPool.UTF_8),
+                "application/json; charset=utf-8")
+            .send();
+        log(log, vars, "================>\n   " + response.bodyText());
+        Boolean success = JsonPath.read(response.bodyText(),
+            "$.success");
+        if (!success
+            || JsonPath.read(response.bodyText(), "$.result") == null) {
+          log(log, vars, "查询流程状态时发生错误");
+          break;
         }
-    }
 
-    public static void log(Logger log, JMeterVariables vars, String msg) {
-        msg = vars.getThreadName() + ": " + msg;
-        log.info(msg);
-        System.out.println(msg);
-    }
-
-    public static void checkState(Logger log, JMeterVariables vars, String path,
-            String flowId, int sleepInterval) {
-        try {
-            while (true) {
-                log(log, vars, "Will be sleeping for the next " + sleepInterval
-                    + " seconds..............");
-                Thread.sleep(sleepInterval * 1000);
-                log(log, vars, "Wakes up now and continues to work");
-                log(log, vars,
-                    String.format("path=[%s],flowId=[%s]", path, flowId));
-
-                JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("id", flowId);
-                HttpResponse response = HttpRequest.post(path)
-                    .header("User-Agent", BshUtils.class.getName())
-                    .body(jsonObject.toString().getBytes(StringPool.UTF_8),
-                        "application/json; charset=utf-8")
-                    .send();
-                log(log, vars, "================>\n   " + response.bodyText());
-                Boolean success = JsonPath.read(response.bodyText(),
-                    "$.success");
-                if (!success
-                    || JsonPath.read(response.bodyText(), "$.result") == null) {
-                    log(log, vars, "查询流程状态时发生错误");
-                    break;
-                }
-
-                String jsonPath = "$.result.flowNodes[0].runtimeState";
-                Integer state = JsonPath.read(response.bodyText(), jsonPath);
-                if (state != 2) {
-                    log(log, vars, "G-SA-WX is still ongoing");
-                    continue;
-                }
-
-                jsonPath = "$.result.flowNodes[1].graph.grapgNodes[2].runtimeState";
-                state = JsonPath.read(response.bodyText(), jsonPath);
-                if (state == 2) {
-                    break;
-                }
-
-                log(log, vars, "J-CREATE-INSTALLMENT-ORDER is still ongoing");
-                continue;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        String jsonPath = "$.result.flowNodes[0].runtimeState";
+        Integer state = JsonPath.read(response.bodyText(), jsonPath);
+        if (state != 2) {
+          log(log, vars, "G-SA-WX is still ongoing");
+          continue;
         }
+
+        jsonPath = "$.result.flowNodes[1].graph.grapgNodes[2].runtimeState";
+        state = JsonPath.read(response.bodyText(), jsonPath);
+        if (state == 2) {
+          break;
+        }
+
+        log(log, vars, "J-CREATE-INSTALLMENT-ORDER is still ongoing");
+        continue;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
 }
--- src/test/java/com/github/binarywang/bsh/BshUtilsTest.java
+++ src/test/java/com/github/binarywang/bsh/BshUtilsTest.java
@@ -1,58 +1,57 @@
 package com.github.binarywang.bsh;
 
-import static org.mockito.Mockito.when;
-
-import java.io.UnsupportedEncodingException;
-
+import com.github.binarywang.BaseTestCase;
+import com.google.common.collect.ImmutableMap;
 import org.apache.jmeter.threads.JMeterVariables;
 import org.apache.log.Hierarchy;
 import org.apache.log.Logger;
 import org.mockito.Mock;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import com.github.binarywang.BaseTestCase;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
 
 /**
  * @author Binary Wang
  */
 public class BshUtilsTest extends BaseTestCase {
-    private Logger log;
+  private Logger log;
 
-    @Mock
-    private JMeterVariables vars;
+  @Mock
+  private JMeterVariables vars;
 
-    @BeforeMethod
-    public void setUp() {
-        this.log = Hierarchy.getDefaultHierarchy().getLoggerFor("BshUtilsTest");
-        when(this.vars.getIterator())
-            .thenReturn(Maps.newHashMap(ImmutableMap.of("a", 1, "b", "uii"))
-                .entrySet().iterator());
-        when(this.vars.getThreadName()).thenReturn("thread name bababa");
-    }
+  @BeforeMethod
+  public void setUp() {
+    this.log = Hierarchy.getDefaultHierarchy().getLoggerFor("BshUtilsTest");
+    Iterator<Map.Entry<String, Object>> iterator = ImmutableMap
+        .of("a", new Object(), "b", new Object())
+        .entrySet().iterator();
+    when(this.vars.getIterator())
+        .thenReturn(iterator);
+    when(this.vars.getThreadName()).thenReturn("thread name bababa");
+  }
 
-    /**
-     * Test method for {@link com.github.binarywang.bsh.BshUtils#printParameters(org.apache.log.Logger, org.apache.jmeter.threads.JMeterVariables)}.
-     */
-    @Test
-    public void testPrintParameters() {
-        BshUtils.printParameters(this.log, this.vars);
-    }
+  /**
+   * Test method for {@link com.github.binarywang.bsh.BshUtils#printParameters(org.apache.log.Logger, org.apache.jmeter.threads.JMeterVariables)}.
+   */
+  @Test
+  public void testPrintParameters() {
+    BshUtils.printParameters(this.log, this.vars);
+  }
 
-    /**
-     * Test method for
-     * {@link com.github.binarywang.bsh.BshUtils#checkState(org.apache.log.Logger, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}
-     * .
-     * 
-     * @throws UnsupportedEncodingException
-     * @throws InterruptedException
-     */
-    @Test
-    public void testCheckState()
-            throws UnsupportedEncodingException, InterruptedException {
-        BshUtils.checkState(this.log, this.vars,
-            "http://10.255.33.103/v1/flow/getstate", "1995", 5);
-    }
+  /**
+   * Test method for
+   * {@link com.github.binarywang.bsh.BshUtils#checkState(Logger, JMeterVariables, String, String, int)}
+   *
+   * @throws UnsupportedEncodingException
+   * @throws InterruptedException
+   */
+  @Test
+  public void testCheckState() {
+    BshUtils.checkState(this.log, this.vars,
+        "http://10.255.33.103/v1/flow/getstate", "1995", 5);
+  }
 }
